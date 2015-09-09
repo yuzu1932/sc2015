49c63e37bf9907c33c596d245b8e575a52549d4e
ZennoFact <zenno04.g@gmail.com>
2015-09-09 10:47:28 +0900

    [Task] confrict_test.js

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-﻿  # サマーコース2015の内容
+# サマーコース2015の内容
+## 9/9 confrict_test.js の課題を追加(締切9/10中)
 
-  ## Day1 JavaScriptを学んだ
-  * オブジェクト指向
-  * クロージャ
-  * オブザーバ
-　* this
+## Day1 JavaScriptを学んだ
+* オブジェクト指向
+* クロージャ
+* オブザーバ
+* this
 
-  ## Day2 チーム開発に便利なツールを学ぶんだ
-  1.GitとGitHub
-  2.Vagrant
+## Day2 チーム開発に便利なツールを学ぶんだ
+1.GitとGitHub
+2.Vagrant
---
+++ confrict_test.js
@@ -0,0 +1,34 @@
+/*
+ * 提出課題です，心の声を作成するプログラムを作成してください。
+ * 「。」「，」の文字を「...」に変換し，「？の後ろ」と，ランダムな位置に「...」を挿入するプログラムを作成しなさい。
+ * とは言っても，４箇所のTODOの部分のコードを書くだけです。コードの中のヒントを見落とさないように。
+ * 提出はpushで行ってもらいますが，いつpushするかはこちらから指示しますので，勝手にpushしないように。
+ * この注意を守れない場合は，提出課題の採点を行いませんので，しっかり守ってください。
+ */
+
+String.prototype.insertDots = function(str, index) {
+  // TODO: この変数の中身を作ってください
+  return　str;
+};
+
+var word = "ゆずる君，ゆずる君，聞こえますか？今，あなたの心に,直接話しかけています。このリポジトリをpullするのです。pullするのです。"
+var myVoice = toVoiceInsideYourHeart(word);
+console.log(myVoice);
+
+function toVoiceInsideYourHeart(phrase) {
+  var max_num = 20;
+  var num = max_num;
+  for (var i = 0; i < phrase.length; i++) {
+    if (phrase.charAt(i) == '。' || phrase.charAt(i) == '，') {
+      phrase = phrase.substring(0, i) + (phrase.substring(i, phrase.length)).replace(phrase.charAt(i), '...');
+      num = max_num;
+      i += 0; // TODO: 0の代わりに，正しい数値を入れてください
+    } else if (phrase.charAt(i - 1) == '？' || Math.floor(Math.random() * num) < -1) {　// TODO: 条件式の右辺の -1 を自分が最適だと感じる数値に調整してください。
+      phrase = phrase.insertDots(phrase, i);
+      num = max_num;
+      i += 0; // TODO: 0の代わりに，正しい数値を入れてください
+    }
+    num--;
+  }
+  return phrase;
+}
