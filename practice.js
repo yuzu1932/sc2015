a2c24e910e1b5b15b4b80f976379be33417d21c6
ZennoFact <zenno04.g@gmail.com>
2015-09-10 19:13:55 +0900

    [Add] add function -> practice.js

--- practice.js
+++ practice.js
@@ -1,7 +1,12 @@
 Number.prototype.plus = function ( num ) {
   return this + num;
 };
+Number.prototype.minus = function ( num ) {
+  return this - num;
+};
 
 var number = 100;
-var result = number.plus(1000);
-console.log(result);
+var result1 = number.plus(1000);
+var result2 = number.minus(10);
+console.log(result1);
+console.log(result2);
